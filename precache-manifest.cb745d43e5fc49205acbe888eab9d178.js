cf440eeb22bc7833619a4491ede502129ca53ae4
vakhno <vakhnohere@gmail.com>
2020-10-24 23:00:48 +0300

    Updates

---
+++ precache-manifest.cb745d43e5fc49205acbe888eab9d178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2f228e20bf4977b62bf4f8506ff4af",
+    "url": "https://dsomething.cloudfront.net/index.html"
+  },
+  {
+    "revision": "a59e29023ce0c32a85e8",
+    "url": "https://dsomething.cloudfront.net/static/css/main.3ae034ce.chunk.css"
+  },
+  {
+    "revision": "07caba821bb298b1ce85",
+    "url": "https://dsomething.cloudfront.net/static/js/2.8b8b8ae6.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "https://dsomething.cloudfront.net/static/js/2.8b8b8ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59e29023ce0c32a85e8",
+    "url": "https://dsomething.cloudfront.net/static/js/main.62505895.chunk.js"
+  },
+  {
+    "revision": "fe0c8a952e02536a9e89",
+    "url": "https://dsomething.cloudfront.net/static/js/runtime-main.a6689f2f.js"
+  }
+]);
