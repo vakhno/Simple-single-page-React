782a684925a9efc605a4163c9e1870449f430cf2
vakhno <vakhnohere@gmail.com>
2020-10-24 22:54:17 +0300

    Added label arror visualization

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom'
 import Input from '../Input/Input.js'
 import Button from '../Button/Button.js'
 import './SignInForm.sass'
+import '../../App.sass'
 
 const SignInForm = ({ onCancel }) => {
 	const [name, setName] = useState('')
@@ -42,8 +43,10 @@ const SignInForm = ({ onCancel }) => {
 
 	return (
 		<form className="user-sign-form" onSubmit={submitForm}>
-			<Input type="text" name="name" placeholder="Your Name" customClass={`user-sign-form__input ${formInputs.name ? '' : 'error-input'}`} inputStyle='input__dark' value={name} onChange={valueToState} />
-			<Input type="password" name="password" placeholder="Your Password" customClass={`user-sign-form__input ${formInputs.password ? '' : 'error-input'}`} inputStyle='input__dark' value={password} onChange={valueToState} />
+			<label htmlFor="userSignInName" className={`user-sign-form__label ${formInputs.name ? '' : 'error-text'}`}>{formInputs.name ? 'Enter name' : 'Enter more than 2 characters'}</label>
+			<Input type="text" name="name" id="userSignInName" customClass={`user-sign-form__input`} inputStyle='input__dark' value={name} onChange={valueToState} />
+			<label htmlFor="userSignInPassword" className={`user-sign-form__label ${formInputs.password ? '' : 'error-text'}`}>{formInputs.password ? 'Enter password' : 'Enter more than 6 characters'}</label>
+			<Input type="password" name="password" id="userSignInPassword" customClass={`user-sign-form__input`} inputStyle='input__dark' value={password} onChange={valueToState} />
 			<Button type={'submit'} customClass="" buttonStyle="button__dark">Subscribe</Button>
 			{formValid &&
 				<>
