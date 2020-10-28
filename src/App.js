f852c89b9d4049c97c74d7be5575856573b676a4
vakhno <vakhnohere@gmail.com>
2020-10-28 06:07:05 +0200

    For gh-pages(test2)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.js'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.sass';
+import './App.sass'
 import Home from './components/pages/Home'
 import SignIn from './components/pages/signIn'
 import error404 from './components/pages/error404'
 
 function App() {
 	return (
-		<>
-			<Router basename="/Simple-single-page-React">
-				<Navbar />
-				<Switch>
-					<Route path='/' exact component={Home} />
-					<Route path='/sign-in' exact component={SignIn} />
-					<Route component={error404} />
-				</Switch>
-			</Router>
-		</>
+		<Router basename="/Simple-single-page-React">
+			<Navbar />
+			<Switch>
+				<Route path='/' exact component={Home} />
+				<Route path='/sign-in' exact component={SignIn} />
+				<Route component={error404} />
+			</Switch>
+		</Router>
 	)
 }
 
